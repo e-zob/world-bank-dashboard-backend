eda165e3a8a30c07fc7b922b1f867be347c1ad4e
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-05-24 21:12:35 +0100

    ready for deployment

--- seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users(id, email, password) VALUES (1, 'alex@sigmalabs.xyz', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', datetime('now'), datetime('now'));
-INSERT INTO users(id, email, password) VALUES (2, 'rich@sigmalabs.xyz', '$2y$12$PEmxrth.vjPDazPWQcLs6u9GRFLJvneUkcf/vcXn8L.bzaBUKeX4W', datetime('now'), datetime('now'));
-INSERT INTO users(id, email, password) VALUES (3, 'yas@sigmalabs.xyz', '$2a$10$​T4ImbDRHK0L/W8o4LfRp8ObdAw.Wtp1kos8pBIG6nlPCUo1ml8jHi', datetime('now'), datetime('now'));
