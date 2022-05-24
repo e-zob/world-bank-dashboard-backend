6cf3cdb2fa31390d3371fcbb7f59a6d5fd135e46
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-05-24 16:10:14 +0100

    refactored code to work with postgres

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ import { abcCors } from "https://deno.land/x/cors/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
 
-// const wb = new Client("postgres://czreijar:TJ2StTuQIl2CoRoinQTwPxk8pBGfdf6t@kandula.db.elephantsql.com/czreijar");
-// await wb.connect();
-// const users = new Client("postgres://dzmurumb:6ZSO9Eo4oiMkZ-GOEYmkwt7fTOXpexW-@kesavan.db.elephantsql.com/dzmurumb");
-// const owner = "dzmurumb";
-// await users.connect();
+const wb = new Client("postgres://czreijar:TJ2StTuQIl2CoRoinQTwPxk8pBGfdf6t@kandula.db.elephantsql.com/czreijar");
+await wb.connect();
+const users = new Client("postgres://dzmurumb:6ZSO9Eo4oiMkZ-GOEYmkwt7fTOXpexW-@kesavan.db.elephantsql.com/dzmurumb");
+await users.connect();
 
-const userdb = new DB("./users.db");
 const app = new Application();
-const PORT = 8080;
 
 app
   .get("/hash/:pass", async (server) => {
@@ -36,13 +33,13 @@ console.log(`Server running on http://localhost:${PORT}`);
 
 async function logIn(server) {
   const { username, password } = await server.body;
-  const [user] = getUser(username);
+  const user = await getUser(username);
   if (!user) return server.json({ response: "User not found" }, 400);
   const encryptedPassword = await bcrypt.hash(password, user.salt);
   const isValidPassword = encryptedPassword === password;
   if (isValidPassword) {
     const sessionId = v4.generate();
-    await userdb.query("INSERT INTO sessions (uuid, user_id, created_at) VALUES (?, ?, datetime('now'))", [sessionId, user.id]);
+    await users.queryArray("INSERT INTO sessions (uuid, user_id, created_at) VALUES (?, ?, datetime('now'))", [sessionId, user.id]);
     server.setCookie({
       name: "sessionId",
       value: sessionId,
@@ -55,51 +52,15 @@ async function createAccount(server) {
   const { username, password } = await server.body;
   const user = await getUser(username);
   if (user) return server.json({ response: "User already exists" }, 400);
-
   const salt = await bcrypt.genSalt();
   const encryptedPassword = await bcrypt.hash(password, salt);
-  const query = `INSERT INTO users (username, password, salt) VALUES (?,?,?)`;
-  await userdb.query(query, [username, encryptedPassword, salt]);
+  const query = `INSERT INTO users (username, password, salt) VALUES ($1,$2,$3)`;
+  await users.queryArray(query, username, encryptedPassword, salt);
   return server.json({ response: "Success" }, 200);
 }
 
 async function getUser(username) {
-  const query = `SELECT * FROM users WHERE username=?`;
-  const [user] = await userdb.query(query, [username]);
+  const query = `SELECT * FROM users WHERE username= $1`;
+  const [user] = (await users.queryObject(query, username)).rows;
   return user;
 }
-
-// async function logIn(server) {
-//   const { username, password } = await server.body;
-//   const query = `SELECT * FROM users WHERE username=$1`;
-//   const user = await users.queryObject(query, [username]).rows;
-//   if (!user) return server.json({ response: "User not found" }, 400);
-//   const encryptedPassword = await bcrypt.hash(password, user.salt);
-//   const isValidPassword = encryptedPassword === password;
-//   if (isValidPassword) {
-//     const sessionId = v4.generate();
-//     await users.queryArray("INSERT INTO sessions (uuid, user_id, created_at) VALUES (?, ?, datetime('now'))", [sessionId, user.id]);
-//     server.setCookie({
-//       name: "sessionId",
-//       value: sessionId,
-//     });
-//   }
-//   return isValidPassword ? server.json({ response: "Success" }, 200) : server.json({ response: "Invalid Password" }, 400);
-// }
-
-// async function createAccount(server) {
-//   const { username, password } = await server.body;
-//   const user = await getUser(username);
-//   if (user) return server.json({ response: "User already exists" }, 400);
-//   const salt = await bcrypt.genSalt();
-//   const encryptedPassword = await bcrypt.hash(password, salt);
-//   const query = `INSERT INTO users (username, password, salt) VALUES ($1,$2,$3)`;
-//   await users.queryArray(query, [username], [encryptedPassword], [salt]);
-//   return server.json({ response: "Success" }, 200);
-// }
-
-// async function getUser(username) {
-//   const query = `SELECT * FROM users WHERE username=$1`;
-//   const user = await users.queryObject(query, [username]).rows;
-//   return user;
-// }
